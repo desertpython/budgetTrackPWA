eb6ca2317621090aed340f28753b688dfc8cce19
Desert Python <mattsenneff@aol.com>
2020-09-30 20:00:44 -0700

    checkDatabase and refactor

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,35 +1,71 @@
 const indexedDB =
-  window.indexedDB ||
-  window.mozIndexedDB ||
-  window.webkitIndexedDB ||
-  window.msIndexedDB ||
-  window.shimIndexedDB;
+    window.indexedDB ||
+    window.mozIndexedDB ||
+    window.webkitIndexedDB ||
+    window.msIndexedDB ||
+    window.shimIndexedDB;
 
-  let db;
-  const request = indexedDB.open("budget", 1);
-  
-  request.onupgradeneeded = ({ target }) => {
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = ({
+    target
+}) => {
     let db = target.result;
-    db.createObjectStore("pending", { autoIncrement: true });
-  };
-  
-  request.onsuccess = ({ target }) => {
+    db.createObjectStore("pending", {
+        autoIncrement: true
+    });
+};
+
+request.onsuccess = ({
+    target
+}) => {
     db = target.result;
-  
+
     // check if app is online before reading from db
     if (navigator.onLine) {
-      checkDatabase();
+        checkDatabase();
     }
-  };
-  
-  request.onerror = function(event) {
-    console.log("Whoops! " + event.target.errorCode);
-  };
+};
 
-  function saveRecord(record) {
+request.onerror = function (event) {
+    console.log("Whoops! " + event.target.errorCode);
+};
+
+function saveRecord(record) {
     const transaction = db.transaction(["pending"], "readwrite");
     const store = transaction.objectStore("pending");
-  
+
     store.add(record);
-  }
-  
+}
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                    method: "POST",
+                    body: JSON.stringify(getAll.result),
+                    headers: {
+                        Accept: "application/json, text/plain, */*",
+                        "Content-Type": "application/json"
+                    }
+                })
+                .then(response => {
+                    return response.json();
+                })
+                .then(() => {
+                    // delete records if successful
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
